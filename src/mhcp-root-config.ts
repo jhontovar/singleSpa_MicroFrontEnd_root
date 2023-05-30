c520782a3d7074157cec41b9a5d49f66ed732ba9
jhon134@hotmail.com <jtovar@asesoftware.com>
2023-05-30 11:27:50 -0500

    Adicion de configuracion

--- src/mhcp-root-config.ts
+++ src/mhcp-root-config.ts
@@ -6,7 +6,20 @@ import {
 } from "single-spa-layout";
 import microfrontendLayout from "./microfrontend-layout.html";
 
-const routes = constructRoutes(microfrontendLayout);
+
+//const routes = constructRoutes(microfrontendLayout);
+
+const htmlTemplate = document.querySelector('#single-spa-template')
+const layoutData = {
+  props: {
+    authToken: "78sf9d0fds89-0fysdiuf6sf8X",
+  },
+  loaders: {
+  }
+};
+
+const routes = constructRoutes(htmlTemplate, layoutData)
+
 const applications = constructApplications({
   routes,
   loadApp({ name }) {
